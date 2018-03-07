121d1532f64d127533886a5cea0d9d2bdab48e04
Eric Smolen <esmolen2@gmail.com>
2018-03-07 08:54:41 -0500

    Revert "Added submit button variable"
    This reverts commit 281a2f0df9a4e337ad8eaac91aa14bc624323938.

--- designs.js
+++ designs.js
@@ -4,10 +4,8 @@ const inputColor = document.querySelector('#colorPicker');
 // Select size input
 const inputHeight = document.querySelector('#inputHeight');
 const inputWidth = document.querySelector('#inputWidth');
-const submitButton = document.querySelector('submit');
 
 // When size is submitted by the user, call makeGrid()
-
 
 function makeGrid() {
 
